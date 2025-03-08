c22996318c2550e471a7f21738b282659b70e26e
Aadesh Kumar <162555223+Aadeshkumar620@users.noreply.github.com>
2025-03-08 13:30:33 +0000

    commit

--- A_6_HeaderFile_And_Opeter.cpp
+++ A_6_HeaderFile_And_Opeter.cpp
@@ -1,11 +1,17 @@
-#include<iomanip>
+#include<iostream>
 
 using namespace std;
 
 int main(){
     int a=4 , b=6;
     int sum = a+b;
-    cout<<sum;
+    cout<<"Opeters is in C++ : \n";
+    cout<<"The value of a+b is "<<a+b<<"\n";
+    cout<<"The value of a-b is "<<a-b<<"\n";
+    cout<<"The value of a*b is "<<a*b<<"\n";
+    cout<<"The value of a/b is "<<a/b<<"\n";
+    cout<<"The value of a%b is "<<a%b<<"\n";
+    
     
     return 0;
 }
